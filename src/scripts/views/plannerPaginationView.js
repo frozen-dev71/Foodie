fc68c31805750c54408cb99a2123c9f0891635d9
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2023-01-05 23:17:32 +0300

    Added paginationPlanner functionality for the Planner page

---
+++ src/scripts/views/plannerPaginationView.js
@@ -0,0 +1,50 @@
+import icons from 'url:../../img/icons.svg';
+import { clearIngredients } from '../model';
+import View from './View';
+
+class PlannerPaginationView extends View {
+  _parentElement = document.querySelector('.planner__pagination');
+
+  addHandlerRenderBtns(handler) {
+    window.addEventListener('load', handler);
+  }
+
+  addHandlerChangeWeek(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--week');
+      if (!btn) return;
+
+      const goToWeek = +btn.dataset.goToWeek;
+      handler(goToWeek);
+    });
+  }
+
+  _generateMarkup() {
+    const currentPage = this._data.page;
+    if (currentPage === 1)
+      return `
+      <button class="btn--week pagination__btn--next" data-go-to-week="${
+        currentPage + 1
+      }">
+        <span>Next week</span>
+        <svg class="search__icon">
+          <use href="${icons}#icon-arrow-right"></use>
+        </svg>
+      </button>
+      `;
+    if (currentPage === 2) {
+      return `
+      <button class="btn--week pagination__btn--prev" data-go-to-week="${
+        currentPage - 1
+      }">
+        <svg class="search__icon">
+          <use href="${icons}#icon-arrow-left"></use>
+        </svg>
+        <span>Current week</span>
+      </button>
+      `;
+    }
+  }
+}
+
+export default new PlannerPaginationView();
