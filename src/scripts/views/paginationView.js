ffeee2124988ca366b1947470b17866fe180a2c7
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2023-01-05 18:02:00 +0300

    Added number of available pages to the Paginationview.js

--- src/scripts/views/paginationView.js
+++ src/scripts/views/paginationView.js
@@ -23,47 +23,60 @@ class PaginationView extends View {
     // Page 1, and there are other pages
     if (curPage === 1 && numPages > 1) {
       return `
-        <button data-goto="${curPage + 1
-        }" class="btn--inline pagination__btn--next">
-          <span>Page ${curPage + 1}</span>
+      <button data-goto="${
+        currentPage + 1
+      }" class="btn--inline pagination__btn--next">
+          <span>Page ${currentPage + 1}</span>
           <svg class="search__icon">
             <use href="${icons}#icon-arrow-right"></use>
           </svg>
-        </button>
-      `;
+      </button>
+      <div class="pagination__label">
+          <span class="pagination__label-text">Page ${currentPage} out of ${numPages}</span>
+      </div>
+    `;
     }
 
     // Last page
     if (curPage === numPages && numPages > 1) {
       return `
-        <button data-goto="${curPage - 1
-        }" class="btn--inline pagination__btn--prev">
+      <button data-goto="${
+        currentPage - 1
+      }" class="btn--inline pagination__btn--prev">
           <svg class="search__icon">
             <use href="${icons}#icon-arrow-left"></use>
           </svg>
-          <span>Page ${curPage - 1}</span>
-        </button>
-      `;
+          <span>Page ${currentPage - 1}</span>
+      </button>
+      <div class="pagination__label">
+          <span class="pagination__label-text">Page ${currentPage} out of ${numPages}</span>
+      </div>
+    `;
     }
 
     // Other page
     if (curPage < numPages) {
       return `
-        <button data-goto="${curPage - 1
-        }" class="btn--inline pagination__btn--prev">
+      <button data-goto="${
+        currentPage - 1
+      }" class="btn--inline pagination__btn--prev">
           <svg class="search__icon">
             <use href="${icons}#icon-arrow-left"></use>
           </svg>
-          <span>Page ${curPage - 1}</span>
-        </button>
-        <button data-goto="${curPage + 1
-        }" class="btn--inline pagination__btn--next">
-          <span>Page ${curPage + 1}</span>
+          <span>Page ${currentPage - 1}</span>
+      </button>
+      <button data-goto="${
+        currentPage + 1
+      }" class="btn--inline pagination__btn--next">
+          <span>Page ${currentPage + 1}</span>
           <svg class="search__icon">
-            <use href="${icons}#icon-arrow-right"></use>
+              <use href="${icons}#icon-arrow-right"></use>
           </svg>
-        </button>
-      `;
+      </button>
+      <div class="pagination__label">
+          <span class="pagination__label-text">Page ${currentPage} out of ${numPages}</span>
+      </div>
+    `;
     }
 
     // Page 1, and there are NO other pages
